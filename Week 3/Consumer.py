980346045e03b987de9def5c2387b317808e69ad
LianD-09 <doanhlinh09@gmail.com>
2022-12-02 16:13:40 +0700

    Report

--- Week 3/Consumer.py
+++ Week 3/Consumer.py
@@ -1,22 +1,30 @@
-import pika, sys, os, json
+import pika
+import sys
+import os
+import json
+
 
 def main():
-    credentials = pika.PlainCredentials(username='doanhlinh', password='20194314', erase_on_connect=True)
-    parameters = pika.ConnectionParameters(host='localhost', port=5672, virtual_host='rabbitmq_linhda', credentials=credentials)
+    credentials = pika.PlainCredentials(
+        username='doanhlinh', password='20194314', erase_on_connect=True)
+    parameters = pika.ConnectionParameters(
+        host='localhost', port=5672, virtual_host='rabbitmq_linhda', credentials=credentials)
 
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     channel.queue_declare(queue='test')
-    
-    # Since RabbitMQ works asynchronously, every time you receive a message, a callback function is called. We will simply print the message body to the terminal 
+
+    # Since RabbitMQ works asynchronously, every time you receive a message, a callback function is called. We will simply print the message body to the terminal
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % json.loads(body))
 
-    channel.basic_consume(queue='test', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(
+        queue='test', on_message_callback=callback, auto_ack=True)
     print('[*] Waiting for messages')
-    
+
     channel.start_consuming()
-    
+
+
 if __name__ == '__main__':
     try:
         main()
